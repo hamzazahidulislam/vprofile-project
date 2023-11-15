248ceb534cbb0c94db5e63530015d64b77db204f
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2023-11-15 15:14:41 +0600

    initial

---
+++ list/local-setup/vagrant/Manual_provisioning/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"8c48d070c39936428a697191895e12d349cb7e4ca4fce1a320b681a6a38abb01","vagrant_version":"2.4.0"}
